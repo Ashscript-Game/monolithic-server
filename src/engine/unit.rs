5573599d088f74829823d0706aab719189a8c3c8
Carson <carsonburke22@gmail.com>
2024-10-08 16:15:17 -0700

    ashscript-types -> 0.4

--- src/engine/unit.rs
+++ src/engine/unit.rs
@@ -1,9 +1,27 @@
-use ashscript_types::unit::Unit;
+use std::u32;
+
+use ashscript_types::unit::{Unit, UnitPart};
 
 pub fn age(unit: &mut Unit) {
     unit.age += 1;
 }
 
 pub fn attack(attacker: &mut Unit, target: &mut Unit) {
-    
+
+}
+
+pub fn unit_range(unit: &Unit) -> u32 {
+    unit.body[UnitPart::Ranged]
+}
+
+pub fn unit_damage(unit: &Unit) -> u32 {
+    unit.body[UnitPart::Ranged]
+}
+
+pub fn unit_attack_cost(unit: &Unit) -> u32 {
+    unit.body[UnitPart::Ranged]
+}
+
+pub fn unit_weight(unit: &Unit) -> u32 {
+    u32::MAX
 }
