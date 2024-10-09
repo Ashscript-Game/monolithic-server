25b2ee2cf8c536926be83059d9ff112898960055
Carson <carsonburke22@gmail.com>
2024-10-08 22:36:47 -0700

    logic to emit game_state

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,35 +1,57 @@
-use std::time::Duration;
+use std::{collections, time::Duration};
 
 use log::info;
 use serde_json::{json, Value};
 use socketioxide::extract::{Data, SocketRef};
 use tokio::time::sleep;
 
-pub async fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
-    info!("New socket connection from: {} - id: {}", socket.ns(), socket.id);
+use crate::game_state::GameState;
 
-    socket.emit("keyframe", json!({
-        "chunk": 1,
-        "units": ["epicUnit1", "epicUnit2", "KillMeNow."],
-        "structures": [{
-            "id": "epicStructure1",
-            "type": "spawn",
-            "position": [40, 20]
-        }, {
-            "id": "epicStructure2",
-            "type": "spawn",
-            "position": [20, 25]
-        }]
-    })).unwrap();
+pub async fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
+    info!(
+        "New socket connection from: {} - id: {}",
+        socket.ns(),
+        socket.id
+    );
+
+    let mut game_state = GameState {
+        units: collections::HashMap::new(),
+        turret: collections::HashMap::new(),
+    };
 
     loop {
-        socket.emit("action", json!({
-            "actions": [{
-                "id": "epicUnit1",
-                "move": 1,
-            }]
-        })).unwrap();
+        socket
+            .emit(
+                "keyframe",
+                serde_json::from_value(game_state)
+            )
+            .unwrap();
 
         sleep(Duration::from_secs(1)).await;
     }
+
+    // socket.emit("keyframe", json!({
+    //     "chunk": 1,
+    //     "units": ["epicUnit1", "epicUnit2", "KillMeNow."],
+    //     "structures": [{
+    //         "id": "epicStructure1",
+    //         "type": "spawn",
+    //         "position": [40, 20]
+    //     }, {
+    //         "id": "epicStructure2",
+    //         "type": "spawn",
+    //         "position": [20, 25]
+    //     }]
+    // })).unwrap();
+
+    // loop {
+    //     socket.emit("action", json!({
+    //         "actions": [{
+    //             "id": "epicUnit1",
+    //             "move": 1,
+    //         }]
+    //     })).unwrap();
+
+    //     sleep(Duration::from_secs(1)).await;
+    // }
 }
