635e998f560a173d14e1a0d9c8729d2d9deac744
Carson <carsonburke22@gmail.com>
2024-10-12 13:15:01 -0700

    functional intent organizer

--- src/engine/intents.rs
+++ src/engine/intents.rs
@@ -1,17 +1,42 @@
-use ashscript_types::intents::{Intent, IntentName, Intents};
+use ashscript_types::{intents::{FactorySpawn, ResourceTransfer, TurretAttack, UnitAttack, UnitMove}, intents::{Intent, IntentName, Intents}};
 use enum_map::{enum_map, EnumMap};
 
-pub type IntentsByAction = EnumMap<IntentName, Vec<Intent>>;
+/* pub type IntentsByAction = EnumMap<IntentName, Vec<Intent>>; */
 
-pub fn organize_intents(intents: &Intents) -> IntentsByAction {
-    let by_action: IntentsByAction = enum_map! {
-        /* IntentName::UnitAttack => vec![],
-        IntentName::UnitMove => vec![],
-        IntentName::TurretAttack => vec![],
-        IntentName::FactorySpawn => vec![],
-        IntentName::ResourceTransfer => vec![], */
-        _ => vec![],
-    };
+#[derive(Default)]
+pub struct IntentsByAction {
+    pub unit_move: Vec<UnitMove>,
+    pub unit_attack: Vec<UnitAttack>,
+    pub turret_attack: Vec<TurretAttack>,
+    pub factory_spawn: Vec<FactorySpawn>,
+    pub resource_transfer: Vec<ResourceTransfer>,
+}
 
+impl IntentsByAction {
+    pub fn new() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+
+    pub fn add_intent(&mut self, intent: Intent) {
+        match intent {
+            Intent::UnitMove(unit_move) => self.unit_move.push(unit_move),
+            Intent::UnitAttack(unit_attack) => self.unit_attack.push(unit_attack),
+            Intent::TurretAttack(turret_attack) => self.turret_attack.push(turret_attack),
+            Intent::FactorySpawn(factory_spawn) => self.factory_spawn.push(factory_spawn),
+            Intent::ResourceTransfer(resource_transfer) => self.resource_transfer.push(resource_transfer),
+        }
+    }
+}
+
+/// Consumes a list of intents and returns a list of intents by action
+pub fn organize_intents(intents: &mut Intents) -> IntentsByAction {
+    let mut by_action = IntentsByAction::default();
+
+    while let Some(intent) = intents.pop() {
+        by_action.add_intent(intent);
+    }
+    
     by_action
 }
