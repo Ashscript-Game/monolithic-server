42fdc182e7e10f3590add2ecf8069200217c54e5
Carson <carsonburke22@gmail.com>
2024-10-14 23:49:52 -0700

    turret action creation

--- src/engine/bots.rs
+++ src/engine/bots.rs
@@ -4,16 +4,16 @@ use uuid::Uuid;
 
 use crate::{ai::{generalist, shared::BotMemory}, game_state::{self, BotGameState, GameState}};
 
-use super::intents::IntentsByAction;
+use super::intents::IntentsByKind;
 
 pub type PlayerScript = fn(&BotGameState, &mut BotMemory) -> Intents;
 pub type PlayerScripts = HashMap<Uuid, PlayerScript>;
 
-pub fn run_bots(game_state: &mut game_state::GameState) -> IntentsByAction {
+pub fn run_bots(game_state: &mut game_state::GameState) -> IntentsByKind {
     let bot_game_state = game_state::BotGameState::new(game_state);
     let bot_scripts = get_player_scripts(game_state);
 
-    let mut intents_by_action = IntentsByAction::new();
+    let mut intents_by_action = IntentsByKind::new();
 
     for (bot_id, script) in bot_scripts {
 
--- src/engine/intents.rs
+++ src/engine/intents.rs
@@ -1,4 +1,11 @@
-use ashscript_types::{intents::{FactorySpawn, ResourceTransfer, TurretAttack, UnitAttack, UnitMove}, intents::{Intent, IntentName, Intents}};
+use ashscript_types::{
+    actions::{self, ActionsByKind},
+    intents::{
+        self, FactorySpawnUnit, Intent, IntentName, Intents, ResourceTransfer, TurretAttack,
+        UnitAttack, UnitMove, UnitSpawnUnit,
+    },
+    objects::Attackable,
+};
 use enum_map::{enum_map, EnumMap};
 
 use crate::game_state::GameState;
@@ -6,17 +13,18 @@ use crate::game_state::GameState;
 use super::bots::run_bots;
 
 /* pub type IntentsByAction = EnumMap<IntentName, Vec<Intent>>; */
-
+/// Probably makes sense to just have bots return this directly, so that we don't have to construct it from the vec
 #[derive(Default)]
-pub struct IntentsByAction {
+pub struct IntentsByKind {
     pub unit_move: Vec<UnitMove>,
     pub unit_attack: Vec<UnitAttack>,
     pub turret_attack: Vec<TurretAttack>,
-    pub factory_spawn: Vec<FactorySpawn>,
+    pub factory_spawn_unit: Vec<FactorySpawnUnit>,
+    pub unit_spawn_unit: Vec<UnitSpawnUnit>,
     pub resource_transfer: Vec<ResourceTransfer>,
 }
 
-impl IntentsByAction {
+impl IntentsByKind {
     pub fn new() -> Self {
         Self {
             ..Default::default()
@@ -35,14 +43,104 @@ impl IntentsByAction {
             Intent::UnitMove(unit_move) => self.unit_move.push(unit_move),
             Intent::UnitAttack(unit_attack) => self.unit_attack.push(unit_attack),
             Intent::TurretAttack(turret_attack) => self.turret_attack.push(turret_attack),
-            Intent::FactorySpawn(factory_spawn) => self.factory_spawn.push(factory_spawn),
-            Intent::ResourceTransfer(resource_transfer) => self.resource_transfer.push(resource_transfer),
+            Intent::FactorySpawnUnit(factory_spawn) => self.factory_spawn_unit.push(factory_spawn),
+            Intent::UnitSpawnUnit(unit_spawn_unit) => self.unit_spawn_unit.push(unit_spawn_unit),
+            Intent::ResourceTransfer(resource_transfer) => {
+                self.resource_transfer.push(resource_transfer)
+            }
         }
     }
 }
 
-pub fn get_and_process_intents(game_state: &mut GameState) {
-    let intents_by_action = run_bots(game_state);
+pub fn get_and_process_intents(game_state: &mut GameState) -> ActionsByKind {
+    let intents_by_kind = run_bots(game_state);
+    let mut actions_by_kind = ActionsByKind::new();
 
-    
+    create_turret_attack_actions(
+        game_state,
+        &intents_by_kind.turret_attack,
+        &mut actions_by_kind,
+    );
+
+    actions_by_kind
+}
+
+fn create_turret_attack_actions(
+    game_state: &mut GameState,
+    intents: &Vec<intents::TurretAttack>,
+    actions_by_kind: &mut ActionsByKind,
+) {
+    for intent in intents.iter() {
+        let (max_damage, cost) = {
+            let Some(turret) = game_state.map.turret_at(&intent.turret_hex) else {
+                continue;
+            };
+
+            let cost = turret.attack_cost();
+            if turret.energy < cost {
+                continue;
+            }
+            
+            (turret.damage(), cost)
+        };
+
+        let damage = match intent.target_kind {
+            Attackable::Unit(..) => {
+                let Some(target) = game_state.map.unit_at_mut(&intent.target_hex) else {
+                    continue;
+                };
+
+                if target.future_health == 0 {
+                    continue;
+                }
+
+                let damage = max_damage.min(target.future_health);
+                target.future_health -= damage;
+
+                damage
+            }
+            Attackable::Factory(..) => {
+                let Some(target) = game_state.map.factory_at_mut(&intent.target_hex) else {
+                    continue;
+                };
+
+                if target.future_health == 0 {
+                    continue;
+                }
+
+                let damage = max_damage.min(target.future_health);
+                target.future_health -= damage;
+
+                damage
+            }
+            Attackable::Turret(..) => {
+                let Some(target) = game_state.map.turret_at_mut(&intent.target_hex) else {
+                    continue;
+                };
+
+                if target.future_health == 0 {
+                    continue;
+                }
+
+                let damage = max_damage.min(target.future_health);
+                target.future_health -= damage;
+
+                damage
+            }
+        };
+
+        let Some(turret) = game_state.map.turret_at_mut(&intent.turret_hex) else {
+            continue;
+        };
+
+        turret.future_energy = 0.max(turret.energy - cost);
+
+        actions_by_kind.turret_attack.push(actions::TurretAttack {
+            turret_hex: intent.turret_hex,
+            target_hex: intent.target_hex,
+            target_kind: intent.target_kind,
+            damage,
+            cost,
+        });
+    }
 }
