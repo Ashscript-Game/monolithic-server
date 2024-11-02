b420b7490215d34a8365fb684f4b7678ed06c42d
Albert Liu <albertymliu@gmail.com>
2024-11-01 22:39:35 -0700

    meh

--- src/engine/client/mod.rs
+++ src/engine/client/mod.rs
@@ -1,66 +1,16 @@
-
-use ashscript_types::keyframe::KeyFrame;
-use log::{error, info};
-use serde_json::Value;
-use socketioxide::{
-    extract::{Data, SocketRef},
-    SocketIo,
-};
-
 use crate::game_state::GameState;
+use ashscript_types::keyframe::KeyFrame;
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    response::IntoResponse,
+};
+use axum_extra::TypedHeader;
+use std::{net::SocketAddr, sync::Arc};
+use tokio::sync::broadcast::Sender;
+//allows to extract the IP of connecting user
+use axum::extract::connect_info::ConnectInfo;
 
-pub async fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
-    info!(
-        "New socket connection from: {} - id: {}",
-        socket.ns(),
-        socket.id
-    );
-
-    let game_state = GameState::new();
-
-    /* loop {
-        socket
-            .emit(
-                "game_state",
-                serde_json::json!({
-                    "map": game_state.map,
-                    "global": game_state.global,
-                })
-            )
-            .unwrap();
-
-        println!("sent game state to client");
-
-        sleep(Duration::from_secs(1)).await;
-    } */
-
-    // socket.emit("keyframe", json!({
-    //     "chunk": 1,
-    //     "units": ["epicUnit1", "epicUnit2", "KillMeNow."],
-    //     "structures": [{
-    //         "id": "epicStructure1",
-    //         "type": "spawn",
-    //         "position": [40, 20]
-    //     }, {
-    //         "id": "epicStructure2",
-    //         "type": "spawn",
-    //         "position": [20, 25]
-    //     }]
-    // })).unwrap();
-
-    // loop {
-    //     socket.emit("action", json!({
-    //         "actions": [{
-    //             "id": "epicUnit1",
-    //             "move": 1,
-    //         }]
-    //     })).unwrap();
-
-    //     sleep(Duration::from_secs(1)).await;
-    // }
-}
-
-pub fn emit_tick(game_state: &GameState, io: &SocketIo) {
+pub fn emit_tick(game_state: &GameState, sender: &mut Sender<Arc<Vec<u8>>>) {
     /* let mut map: HashMap<Hex, Hex> = HashMap::new();
     map.insert(hex(0, 0), hex(0, 0));
 
@@ -80,36 +30,57 @@ pub fn emit_tick(game_state: &GameState, io: &SocketIo) {
 
     // Emit the actions that have happened since
 
-    io.of("/client")
-        .unwrap()
-        .emit(
-            "game_state",
-            ser_keyframe,
-            
-            /* serde_json::json!({
-                "map": game_state.map,
-                "global": game_state.global,
-                "hash": map,
-            } */
-        )
-        .unwrap();
+    match sender.send(Arc::new(ser_keyframe)) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("{:?}", e);
+        }
+    }
 
     println!("tick emit");
 }
 
-pub fn basic_emit(game_state: &mut GameState, io: &SocketIo) {
-    io.of("/client")
-        .unwrap()
-        .emit(
-            "game_state",
-            serde_json::json!({
-                "key": "value"
-                /* "map": game_state.map,
-                "global": game_state.global, */
-            }),
-        )
-        .unwrap();
-
-    println!("basic emit");
+/// The handler for the HTTP request (this gets called when the HTTP request lands at the start
+/// of websocket negotiation). After this completes, the actual switching from HTTP to
+/// websocket protocol will occur.
+/// This is the last point where we can extract TCP/IP metadata such as IP address of the client
+/// as well as things from HTTP headers such as user-agent of the browser etc.
+pub async fn ws_handler(
+    ws: WebSocketUpgrade,
+    user_agent: Option<TypedHeader<headers::UserAgent>>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    receiver: tokio::sync::broadcast::Receiver<Arc<Vec<u8>>>,
+) -> impl IntoResponse {
+    let user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
+        user_agent.to_string()
+    } else {
+        String::from("Unknown browser")
+    };
+    println!("`{user_agent}` at {addr} connected.");
+    // finalize the upgrade process by returning upgrade callback.
+    // we can customize the callback by sending additional info such as address.
+    ws.on_upgrade(move |socket| handle_socket(socket, receiver))
 }
 
+/// Actual websocket statemachine (one will be spawned per connection)
+async fn handle_socket(
+    mut socket: WebSocket,
+    mut receiver: tokio::sync::broadcast::Receiver<Arc<Vec<u8>>>,
+) {
+    loop {
+        let value = receiver.recv().await;
+        let value = match value {
+            Err(e) => {
+                println!("{:?}", e);
+                continue;
+            }
+            Ok(v) => v,
+        };
+
+        // UGH WHY DOES THIS NEED A FULL VEC
+        match socket.send(Message::Binary((*value).clone())).await {
+            Err(e) => println!("{:?}", e),
+            Ok(_) => {}
+        }
+    }
+}
